d9e71b91406b16a9ebc791e666eaa931ebe7a2dc
Hugh Kristiansen <kaarelinden@hotmail.com>
2021-07-20 21:03:57 +0100

    apads1q232

--- utils/create-database.js
+++ utils/create-database.js
@@ -6,8 +6,7 @@ const path = require('path');
 const args = process.argv.slice(2)[0];
 
 const envFile = args === 'test' ? '../.env.test' : '../.env';
-
-require('dotenv').config({
+process.env.CLEARDB_DATABASE_URL || require('dotenv').config({
   path: path.join(__dirname, envFile),
 });
 
